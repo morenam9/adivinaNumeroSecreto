97690260bc135b98afe4e39e7f470632f856f93b
Morena Machado <67176359+morenam9@users.noreply.github.com>
2023-12-01 18:03:59 -0300

    Update adivinaNumero.js
    Se actualiza lógica de la función verificarAdivinanza.

--- adivinaNumero.js
+++ adivinaNumero.js
@@ -2,12 +2,12 @@ const generarNumeroAleatorio = () => {
     return Math.floor(Math.random() * 100) + 1;
     };
     const verificarAdivinanza = (numeroSecreto, numeroAdivinado) => {
-    if (numeroAdivinado === numeroSecreto) {
-    console.log('¡Felicitaciones! ¡Adivinaste el número secreto!');
+    if (numeroAdivinado <  numeroSecreto) {
+    console.log('El número secreto es mayor. ¡Sigue intentando!');
     } else if (numeroAdivinado > numeroSecreto) {
     console.log('El número secreto es menor. ¡Sigue intentando!');
     } else {
-    console.log('El número secreto es mayor. ¡Sigue intentando!');
+    console.log('¡Felicitaciones! ¡Adivinaste el número secreto!');
     }
     };
     
